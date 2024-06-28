e7dfc21a65855d3255c12ca9f5d64282fa43bff8
Edward Mike <greenpython.developer@gmail.com>
2024-06-28 12:01:26 +0100

    code formatted

--- config/settings.py
+++ config/settings.py
@@ -16,26 +16,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def setup_logger(log_directory, enable_console=False):
-    
+
     class CustomFormatter(logging.Formatter):
         def formatTime(self, record, datefmt=None):
-            return datetime.fromtimestamp(record.created).strftime('%b %d %I:%M:%S %p')
+            return datetime.fromtimestamp(record.created).strftime("%b %d %I:%M:%S %p")
 
     if not os.path.exists(log_directory):
         os.makedirs(log_directory)
 
     log_file = os.path.join(log_directory, os.getenv("LOG_FILE", "default.log"))
-    
+
     file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(CustomFormatter("%(asctime)s - %(levelname)s - %(message)s"))
+    file_handler.setFormatter(
+        CustomFormatter("%(asctime)s - %(levelname)s - %(message)s")
+    )
 
     logging.basicConfig(level=logging.INFO, handlers=[file_handler])
-    
+
     if enable_console:
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(CustomFormatter("%(asctime)s - %(levelname)s - %(message)s"))
+        console_handler.setFormatter(
+            CustomFormatter("%(asctime)s - %(levelname)s - %(message)s")
+        )
         logging.getLogger().addHandler(console_handler)
 
 
